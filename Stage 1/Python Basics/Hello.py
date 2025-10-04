a8c8c4924a402be5003174cfb94be057039e7a8c
Nuraia Tasneem Noha <nuraiatasneem.3g@gmail.com>
2025-10-04 16:18:14 +0600

    Basic printing and data types done

--- Stage 1/Python Basics/Hello.py
+++ Stage 1/Python Basics/Hello.py
@@ -7,3 +7,20 @@ a = int(input("What's the value of a? "))
 b = int(input("What's the value of b? "))
 c = (a**2+b**2)**0.5
 print ("The value of the hypotenuse from the given values is",c)
+
+#Print Name and age
+Name = input("What's your name? ")
+Age = int(input("What's your age? "))
+print ("Your name is",Name,"and your age is",str(Age))
+
+#Two numbers sum
+Num1 = float(input("What's the first number? "))
+Num2= float(input("What's the second number? "))
+Total = Num1 + Num2
+print ("The sum of the two provided numbers is",Total)
+
+#BMI Calc
+Weight = float(input("What's your weight in kg? "))
+Height = float(input("What's your height in m? "))
+BMI = Weight/((Height)**2)
+print ("Your BMI is",str(BMI))
