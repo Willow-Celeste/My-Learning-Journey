7bebcfdfbb0bdca95eefcdcfc3331b9562bab3c9
Nuraia Tasneem Noha <nuraiatasneem.3g@gmail.com>
2025-10-04 15:05:19 +0600

    Initial Commit. Adds the Stage 1 Folder

---
+++ Stage 1/Python Basics/Hello.py
@@ -0,0 +1,9 @@
+print ('Hello World!!') #This is a basic print command
+Name = "Willow" #A Variable
+print (Name)
+
+#Pytha's Formula
+a = int(input("What's the value of a? "))
+b = int(input("What's the value of b? "))
+c = (a**2+b**2)**0.5
+print ("The value of the hypotenuse from the given values is",c)
